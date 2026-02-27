87d54e71a0efff1b95968fd78aeba0e3150a1064
Pedro Rico <pedro.rico@fastcode.today>
2026-02-26 22:06:53 -0600

    Implementacion de metodos cliente

--- tienda/src/main/java/com/inndata20/tienda/controller/ClienteController.java
+++ tienda/src/main/java/com/inndata20/tienda/controller/ClienteController.java
@@ -1,4 +1,23 @@
 package com.inndata20.tienda.controller;
 
+import com.inndata20.tienda.entity.ClienteEntity;
+import com.inndata20.tienda.service.implementacion.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
+
 public class ClienteController {
+    @Autowired
+    ClienteService clienteService;
+
+    @GetMapping("/clientes")
+    public List<ClienteEntity> readAll() {
+        return clienteService.readAll();
+    }
 }
--- tienda/src/main/java/com/inndata20/tienda/service/implementacion/ClienteService.java
+++ tienda/src/main/java/com/inndata20/tienda/service/implementacion/ClienteService.java
@@ -1,4 +1,22 @@
 package com.inndata20.tienda.service.implementacion;
 
-public class ClienteService {
+
+import com.inndata20.tienda.entity.ClienteEntity;
+import com.inndata20.tienda.repository.ClienteRepository;
+import com.inndata20.tienda.service.IClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+
+public class ClienteService implements IClienteService {
+    @Autowired
+    ClienteRepository clienteRepository;
+
+    @Override
+    public List<ClienteEntity> readAll() {
+        return clienteRepository.findAll();
+    }
 }
