b9598856b0ffb5a444b2e7d75bf419b41b0cf418
Brandon-Visoso <kyudaeu2099@gmail.com>
2026-02-28 15:02:20 -0600

    Realizo las rutas en controller

--- tienda/src/main/java/com/inndata20/tienda/controller/EmpleadoController.java
+++ tienda/src/main/java/com/inndata20/tienda/controller/EmpleadoController.java
@@ -16,9 +16,34 @@ public class EmpleadoController {
     @Autowired
     public IEmpleadoService empleadoService;
 
+    // SELECCIONAR TODOS LOS EMPLEADOS
+
     @GetMapping("/listar")
     public List<EmpleadoEntity> listarEmpleados() {
         return empleadoService.listarEmpleados();
     }
 
+}
+
+// BUSCAR POR ID
+
+@GetMapping("/buscar/{id}")
+public EmpleadoEntity buscarPorId(@PathVariable Integer id){
+
+    return empleadoService.buscarPorId(id);
+
+}
+
+// GUARDA UNA NUEVO EMPLEADO
+
+@PostMapping("/guardar")
+public EmpleadoEntity guardarEmpleado(@RequestBody EmpleadoEntity empleado) {
+    return empleadoService.guardarEmpleado(empleado);
+}
+
+// ACTUALIZA UNA PERSONA EXISTENTE
+
+@PutMapping("/actualizar/{id}")
+public EmpleadoEntity actualizarEmpleado(@PathVariable Integer id, @RequestBody EmpleadoEntity empleado) {
+    return empleadoService.actualizarEmpleado(id, empleado);
 }
