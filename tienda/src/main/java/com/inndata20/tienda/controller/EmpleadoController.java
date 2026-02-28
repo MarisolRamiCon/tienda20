786970f5f382b3c30ed1917cf254b69cd2222fdf
Brandon-Visoso <kyudaeu2099@gmail.com>
2026-02-28 15:08:46 -0600

    Arregle problema de llave

--- tienda/src/main/java/com/inndata20/tienda/controller/EmpleadoController.java
+++ tienda/src/main/java/com/inndata20/tienda/controller/EmpleadoController.java
@@ -1,14 +1,3 @@
-package com.inndata20.tienda.controller;
-
-import com.inndata20.tienda.entity.EmpleadoEntity;
-import com.inndata20.tienda.service.IEmpleadoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
 @RestController
 @RequestMapping("/Empleados")
 public class EmpleadoController {
@@ -17,33 +6,27 @@ public class EmpleadoController {
     public IEmpleadoService empleadoService;
 
     // SELECCIONAR TODOS LOS EMPLEADOS
-
     @GetMapping("/listar")
     public List<EmpleadoEntity> listarEmpleados() {
         return empleadoService.listarEmpleados();
     }
 
-}
+    // BUSCAR POR ID
+    @GetMapping("/buscar/{id}")
+    public EmpleadoEntity buscarPorId(@PathVariable Integer id){
+        return empleadoService.buscarPorId(id);
+    }
 
-// BUSCAR POR ID
+    // GUARDA UNA NUEVO EMPLEADO
+    @PostMapping("/guardar")
+    public EmpleadoEntity guardarEmpleado(@RequestBody EmpleadoEntity empleado) {
+        return empleadoService.guardarEmpleado(empleado);
+    }
 
-@GetMapping("/buscar/{id}")
-public EmpleadoEntity buscarPorId(@PathVariable Integer id){
+    // ACTUALIZA UNA PERSONA EXISTENTE
+    @PutMapping("/actualizar/{id}")
+    public EmpleadoEntity actualizarEmpleado(@PathVariable Integer id, @RequestBody EmpleadoEntity empleado) {
+        return empleadoService.actualizarEmpleado(id, empleado);
+    }
 
-    return empleadoService.buscarPorId(id);
-
-}
-
-// GUARDA UNA NUEVO EMPLEADO
-
-@PostMapping("/guardar")
-public EmpleadoEntity guardarEmpleado(@RequestBody EmpleadoEntity empleado) {
-    return empleadoService.guardarEmpleado(empleado);
-}
-
-// ACTUALIZA UNA PERSONA EXISTENTE
-
-@PutMapping("/actualizar/{id}")
-public EmpleadoEntity actualizarEmpleado(@PathVariable Integer id, @RequestBody EmpleadoEntity empleado) {
-    return empleadoService.actualizarEmpleado(id, empleado);
-}
+} // <-- esta llave va hasta el final
