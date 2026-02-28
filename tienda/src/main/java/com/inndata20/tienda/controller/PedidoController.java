c7354d6a64a5659bd799d9f39bb8d42b7680ae15
Brandon-Visoso <kyudaeu2099@gmail.com>
2026-02-28 16:07:39 -0600

    Termino los controller con sus metodos

--- tienda/src/main/java/com/inndata20/tienda/controller/PedidoController.java
+++ tienda/src/main/java/com/inndata20/tienda/controller/PedidoController.java
@@ -3,20 +3,41 @@ package com.inndata20.tienda.controller;
 import com.inndata20.tienda.entity.PedidoEntity;
 import com.inndata20.tienda.service.implementacion.PedidoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/pedidos")
 public class PedidoController {
     @Autowired
     PedidoService pedidoService;
 
-    @GetMapping("/pedidos")
+    @GetMapping("/listar")
     public List<PedidoEntity> readAll(){
         return pedidoService.readAll();
     }
+
+    // BUSCA POR ID
+
+    @GetMapping("/buscar/{id}")
+    public PedidoEntity buscarPorId(@PathVariable Integer id) {
+        return pedidoService.buscarPorId(id);
+    }
+
+    // GUARDA UN PEDIDO
+
+    @PostMapping("/guardar")
+    public PedidoEntity guardarPersona(@RequestBody PedidoEntity persona) {
+        return pedidoService.guardarPersona(persona);
+    }
+
+    // ACTUALIZA UN PEDIDO EXISTENTE
+
+    @PutMapping("/actualizar/{id}")
+    public PedidoEntity actualizarPersona(@PathVariable Integer id, @RequestBody PedidoEntity persona) {
+        return pedidoService.actualizarPedido(id, persona);
+    }
+
+
 }
