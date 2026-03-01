96a223988b64775393caf8f7c46b6433b2ec4249
Brandon-Visoso <kyudaeu2099@gmail.com>
2026-03-01 08:58:47 -0600

    He terminado el controller

---
+++ tienda/src/main/java/com/inndata20/tienda/controller/ProductoController.java
@@ -0,0 +1,43 @@
+package com.inndata20.tienda.controller;
+
+import com.inndata20.tienda.entity.ProductoEntity;
+import com.inndata20.tienda.service.implementacion.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/productos")
+public class ProductoController {
+    @Autowired
+    ProductoService productoService;
+
+    @GetMapping("/listar")
+    public List<ProductoEntity> listarProductos(){
+        return productoService.listarProductos();
+    }
+
+    // BUSCA POR ID
+
+    @GetMapping("/buscar/{id}")
+    public ProductoEntity buscarPorId(@PathVariable Integer id) {
+        return productoService.buscarPorId(id);
+    }
+
+    // GUARDA UN PEDIDO
+
+    @PostMapping("/guardar")
+    public ProductoEntity guardarPersona(@RequestBody ProductoEntity producto) {
+        return productoService.guardarProducto(producto);
+    }
+
+    // ACTUALIZA UN PEDIDO EXISTENTE
+
+    @PutMapping("/actualizar/{id}")
+    public ProductoEntity actualizarProducto(@PathVariable Integer id, @RequestBody ProductoEntity producto) {
+        return productoService.actualizarProducto(id, producto);
+    }
+
+
+}
