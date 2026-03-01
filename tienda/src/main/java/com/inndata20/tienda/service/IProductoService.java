3ab95831b33da94b6caad76e8ebd93bc03497a67
Brandon-Visoso <kyudaeu2099@gmail.com>
2026-03-01 08:46:13 -0600

    Corrigo mi interface de Producto

--- tienda/src/main/java/com/inndata20/tienda/service/IProductoService.java
+++ tienda/src/main/java/com/inndata20/tienda/service/IProductoService.java
@@ -1,15 +1,13 @@
 package com.inndata20.tienda.service;
 
 import com.inndata20.tienda.entity.ProductoEntity;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 public interface IProductoService {
 
     List<ProductoEntity> listarProductos();
-    ProductosEntity buscarPorId(Integer id);
-    ProductosEntity guardarEmpleado(ProductosEntity productos);
-    ProductosEntity actualizarEmpleado(Integer id, ProductosEntity productos);
+    ProductoEntity buscarPorId(Integer id);
+    ProductoEntity guardarProducto(ProductoEntity producto);
+    ProductoEntity actualizarProducto(Integer id, ProductoEntity producto);
 
 }
