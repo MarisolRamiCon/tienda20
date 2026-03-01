2df8db6676276a1768f9821a5fc99b990fc22a08
Brandon-Visoso <kyudaeu2099@gmail.com>
2026-03-01 08:44:34 -0600

    Agrego la implementacion de metodos de interface

--- tienda/src/main/java/com/inndata20/tienda/service/implementacion/ProductoService.java
+++ tienda/src/main/java/com/inndata20/tienda/service/implementacion/ProductoService.java
@@ -1,4 +1,47 @@
 package com.inndata20.tienda.service.implementacion;
 
-public class ProductoService {
+import com.inndata20.tienda.entity.ProductoEntity;
+import com.inndata20.tienda.repository.ProductoRepository;
+import com.inndata20.tienda.service.IProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductoService implements IProductoService {
+
+    @Autowired
+    ProductoRepository productoRepository;
+
+    @Override
+    public List<ProductoEntity> listarProductos() {
+        return productoRepository.findAll();
+    }
+
+    @Override
+    public ProductoEntity buscarPorId(Integer id){
+        return productoRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public ProductoEntity guardarProducto(ProductoEntity producto){
+        return productoRepository.save(producto);
+    }
+
+    @Override
+    public ProductoEntity actualizarProducto(Integer id, ProductoEntity producto) {
+        ProductoEntity productoExistente = productoRepository.findById(id).orElse(null);
+        if (productoExistente != null) {
+            productoExistente.setNombre(producto.getNombre());
+            productoExistente.setDescripcion(producto.getDescripcion());
+            productoExistente.setPrecio(producto.getPrecio());
+            productoExistente.setCategoria(producto.getCategoria());
+            productoExistente.setProveedor(producto.getProveedor());
+            productoExistente.setStock(producto.getStock());
+            return productoRepository.save(productoExistente);
+        }
+        return null;
+    }
+
 }
