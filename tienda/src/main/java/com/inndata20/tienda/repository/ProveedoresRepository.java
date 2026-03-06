22d3f8c98ad176cfd24ac1a88dcfa0bb92c53443
Pedro Rico <pedro.rico@fastcode.today>
2026-03-05 21:51:41 -0600

    Correccion de errores en proveedores

---
+++ tienda/src/main/java/com/inndata20/tienda/repository/ProveedoresRepository.java
@@ -0,0 +1,7 @@
+package com.inndata20.tienda.repository;
+
+import com.inndata20.tienda.entity.ProveedoresEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProvedoresRepository extends JpaRepository<ProveedoresEntity,Integer> {
+}
